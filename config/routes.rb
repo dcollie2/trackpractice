3ceadc78c8e6ed0deea1973a580a95849ca75415
Danny Collier <danny.collier@gmail.com>
2023-08-06 15:53:26 -0400

    MPV working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :pages
   devise_for :users
   resources :practices
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "practices#index"
+  root "pages#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_180742) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_182624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pages", force: :cascade do |t|
+    t.string "name"
+    t.text "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "practices", force: :cascade do |t|
     t.datetime "practice_date"
