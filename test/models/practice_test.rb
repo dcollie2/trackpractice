e68a4febc042d02c911c70665d4fb69e5cd24cca
Danny Collier <danny.collier@gmail.com>
2023-08-13 20:53:46 -0400

    Begin bringing tests up to speed.

--- test/models/practice_test.rb
+++ test/models/practice_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PracticeTest < ActiveSupport::TestCase
+
+  setup do
+    @user =User.create()
+  end
   # test "the truth" do
   #   assert true
   # end
+  # test "should not save without user" do
+  #   practice = Practice.new
+  #   assert_not practice.save
+  # end
+
+  test "should save with a user" do
+    practice = Practice.new(user: @user)
+    assert practice.save
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:testy)
+  end
+
+  test "it should save" do
+    assert @user.save
+  end
+
+  test "user should have practices" do
+    assert @user.practices.size > 0
+  end
 end
