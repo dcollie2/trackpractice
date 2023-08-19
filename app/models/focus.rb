d36a96c89b92fcd96a3f2e4dfc74f82a9e0dcdcd
Danny Collier <danny.collier@gmail.com>
2023-08-19 19:57:21 -0400

    Add foci totals.

--- app/models/focus.rb
+++ app/models/focus.rb
@@ -1,4 +1,9 @@
 class Focus < ApplicationRecord
   belongs_to :user
   has_many :practices
+
+  # total practice time for this focus
+  def total_practice_time
+    practices.present? ? practices.sum(:minutes) : 0
+  end
 end
