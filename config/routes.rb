9076b7f2bf6022bb3cf3855910aa9b92f7876b63
Danny Collier <danny.collier@gmail.com>
2023-08-20 12:19:33 -0400

    Add user management.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :foci
   resources :pages
   devise_for :users
+  resources :users, except: [:create, :new]
   resources :practices
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
