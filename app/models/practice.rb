374ea6cbd6c6d5614ba265aec4fbac6f9d3555d0
Danny Collier <danny.collier@gmail.com>
2023-08-23 12:28:01 -0400

    Fix weekday grouping so we get an accurate practice days count.

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -7,8 +7,8 @@ class Practice < ApplicationRecord
   validates :notes, length: { maximum: 1000 }
 
   scope :this_week, -> { where("practice_date >= ?", Date.today - 7.days) }
-  # get all practices for this week, grouped by date, and sum the minutes for each date
-  scope :this_week_grouped, -> { this_week.group("DATE(practice_date)").sum(:minutes) }
+  # get all practices for this week, grouped by day, and sum the minutes for each day
+  scope :this_week_grouped, -> { this_week.group("to_char(DATE(practice_date), 'DAY')").sum(:minutes) }
   # get total sum of minutes for this week
   scope :this_week_total, -> { this_week.sum(:minutes) }
   # get earliest practice_date
