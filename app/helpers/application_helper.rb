4be683a24d6e842425bdb67cdab5c12dcaca1eff
Danny Collier <danny.collier@gmail.com>
2023-08-25 21:44:13 -0400

    Bleh.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,4 +85,19 @@ module ApplicationHelper
     end
   end
 
+  def flash_class(name)
+    case name
+      when 'notice' then 'alert alert-info alert-dismissable fade show'
+      when 'success' then 'alert alert-success alert-dismissable fade show'
+      when 'error' then 'alert alert-danger alert-dismissable fade show'
+      when 'alert' then 'alert alert-danger alert-dismissable fade show'
+      else
+        'alert alert-primary alert-dismissable fade show'
+    end
+  end
+
+  # Exlude these messages from appearing
+  def flash_blacklist(name)
+    %w[timedout].include?(name)
+  end
 end
