2052a39bca9b8c65461916be1217ba9296730ddd
Danny Collier <danny.collier@gmail.com>
2023-09-01 13:15:17 -0400

    Fix favicon.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,5 @@ import "popper"
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_directory ../stylesheets .css
+//= link_tree ../images
