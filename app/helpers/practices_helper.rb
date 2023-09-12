252624fc0ffa7ca2e30584e20af8fc085b1eb05e
Danny Collier <danny.collier@gmail.com>
2023-09-11 22:36:57 -0400

    Try to fix date oddity.

--- app/helpers/practices_helper.rb
+++ app/helpers/practices_helper.rb
@@ -3,9 +3,9 @@ module PracticesHelper
   def day_background(date, practices)
     if practices.length > 0
       "bg-success"
-    elsif date == Date.today
+    elsif date == Date.current.in_time_zone(current_user.time_zone).to_date
       "bg-warning"
-    elsif date > Date.today
+    elsif date > Date.current.in_time_zone(current_user.time_zone).to_date
       "bg-secondary"
     else
       "bg-danger"
