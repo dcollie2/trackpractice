09d9429bae8d2e389a56d08f01ad4f9ccef44a57
Danny Collier <danny.collier@gmail.com>
2023-09-15 21:09:07 -0400

    Add user streak.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,27 @@ class User < ApplicationRecord
 
   # add a public scope
   scope :with_public_practices, -> { where(make_practices_public: true) }
+
+  def streak
+    # return the count of days practiced without a break
+    streak = 0
+    # get all practices for this user, in descending order by practice_date, grouped by distinct practice_date.to_date
+    # then loop through each practice_date.to_date
+    # for each practice_date.to_date, if the next practice_date.to_date is the day after, increment the streak
+    # if the next practice_date.to_date is not the day after, break the loop
+    # return the streak
+    practices.order(practice_date: :desc).group_by(&:practice_date).each_with_index do |(date, practices), index|
+      practice_date = date.to_date
+      break if index == 0 && practice_date != Date.today
+
+      if index == 0 && practice_date == Date.today
+        streak += 1
+      elsif index > 0 && practice_date == Date.today - index.days
+        streak += 1
+      else
+        break
+      end
+    end
+    streak
+  end
 end
