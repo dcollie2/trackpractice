b2e4bd836961d531ca49ad6ff5acd8b6b0e7023f
Danny Collier <danny.collier@gmail.com>
2023-09-16 09:55:30 -0400

    Fix current day streak bug.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,25 +15,42 @@ class User < ApplicationRecord
   def streak
     # return the count of days practiced without a break
     streak = 0
+
+    # get user's practies in descending order by practice_date
+    user_practices = practices.order(practice_date: :desc)
+
+    # we're starting either today or yesterday
+    today = Time.now.in_time_zone(time_zone).to_date
+    starting_date = user_practices.first.practice_date.in_time_zone(time_zone).to_date
+    logger.debug "today: #{today} - starting_date: #{starting_date}"
+
+    # if practice is older than yesterday, return 0
+    return 0 if starting_date < today - 1.day
+
     # get all practices for this user, in descending order by practice_date, grouped by distinct practice_date.to_date
     # then loop through each practice_date.to_date
     # for each practice_date.to_date, if the next practice_date.to_date is the day after, increment the streak
     # if the next practice_date.to_date is not the day after, break the loop
     # return the streak
-    today = DateTime.current.in_time_zone(time_zone).to_date
-
-    practices.order(practice_date: :desc).group_by { |p| p.practice_day(time_zone) }.each_with_index do |(date, practices), index|
+    user_practices.group_by { |p| p.practice_day(time_zone) }.each_with_index do |(date, practices), index|
       practice_date = date.in_time_zone(time_zone).to_date
-      Rails.logger.debug "practice_date: #{practice_date}"
+      Rails.logger.debug "practice_date: #{starting_date} - today: #{today} - index: #{index}"
 
-      if index <= 0 && (practice_date == today || practice_date == today - 1.day)
+      if index == 0 && starting_date >= today - 1.day
+        # practiced on starting date
         streak += 1
-        Rails.logger.debug "streak: #{streak} - FIRST CONDITION"
-      elsif index > 0 && practice_date == today - index.days
+        Rails.logger.debug "streak: #{streak} - Today CONDITION"
+      elsif index == 0 && practice_date == starting_date - 1.day
+        # didn't practice on starting date but did day before
         streak += 1
-        Rails.logger.debug "streak: #{streak} - SECOND CONDITION"
+        Rails.logger.debug "streak: #{streak} - Yesterday CONDITION"
+      elsif index > 0 && practice_date == starting_date - index.days
+        # another consecuetive day
+        streak += 1
+        Rails.logger.debug "streak: #{streak} - Continuing CONDITION"
       else
-        Rails.logger.debug "streak: #{streak} - BREAKING - #{practice_date} != #{today - index.days}"
+        # broke the stres
+        Rails.logger.debug "streak: #{streak} - BREAKING - #{starting_date} != #{starting_date - index.days}"
         break
       end
     end
