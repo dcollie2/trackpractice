e3ee11af2218c8feb2d7c585d1a5f1ca32e26db2
Danny Collier <danny.collier@gmail.com>
2023-10-20 20:10:28 -0400

    Upgrade to Rails 7.1.1

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,13 @@ module Trackpractice
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+
+    config.active_support.cache_format_version = 7.0
+    config.active_support.disable_to_s_conversion = true
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    #
     config.active_record.default_timezone = :utc
     config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
