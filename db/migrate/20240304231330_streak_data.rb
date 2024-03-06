18bc024d00299230a7fe496510a63edf89afea39
Danny Collier <danny.collier@gmail.com>
2024-03-05 22:02:49 -0500

    Update gems.

--- db/migrate/20240304231330_streak_data.rb
+++ db/migrate/20240304231330_streak_data.rb
@@ -2,6 +2,7 @@ class StreakData < ActiveRecord::Migration[7.1]
   def up
     User.all.each do |user|
       user.practices.order(:practice_date).each do |practice|
+        practice.update!(minutes: 5) if practice.minutes.blank? || practice.minutes < 1
         practice.set_streak_beginning!
       end
     end
