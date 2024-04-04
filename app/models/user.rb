9042685f295aba5eb6490b217168ef88be0c7323
Danny Collier <danny.collier@gmail.com>
2024-04-04 07:29:15 -0400

    Fix user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,7 @@ class User < ApplicationRecord
 
   def most_recent_practice
     practice = practices.order(practice_date: :desc).first
-    practice_days_ago = (Time.zone.now.to_date - practice.practice_date.to_date).to_i
-    practice_days_ago <= 2 ? practice : nil
+    practice.practice_date > 1.day.ago ? nil : practice
   end
 
   def streak
