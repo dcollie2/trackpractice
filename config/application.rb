5a8c21a9a2804602ef2921367a7272e0ecb94c50
Danny Collier <294724+dcollie2@users.noreply.github.com>
2025-01-20 10:01:56 -0500

    Upgrade to rails 8.0.1

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +9,12 @@ Bundler.require(*Rails.groups)
 module Trackpractice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     config.active_support.cache_format_version = 7.0
@@ -22,12 +25,13 @@ module Trackpractice
     config.middleware.use Rack::Deflater
     config.middleware.use Rack::Brotli
 
-    # These settings can be overridden in specific environments using the files
+    #    # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    # config.time_zone = "Central Time (US & Canada)"
     config.active_record.default_timezone = :utc
     config.time_zone = 'Central Time (US & Canada)'
-    # config.eager_load_paths << Rails.root.join("extras")
-    config.add_autoload_paths_to_load_path = false
+
+#    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
